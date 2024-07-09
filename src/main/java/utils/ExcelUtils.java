6ec14df610f2991e4f742bdfaa145e0ea4452b76
srkr.sou.09 <srkr.sou.09@outlook.com>
2024-07-09 23:24:07 +0530

    Reporting is added along with the necessary changes

---
+++ src/main/java/utils/ExcelUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ExcelUtils {
+
+    public static  List<LinkedHashMap<String,String>> getExcelDataAsLsitOfMap(String excelFileName, String sheetName){
+        List<LinkedHashMap<String,String>> dataFromExcel = new ArrayList<>();
+
+        WorkbookFactory.create
+
+
+
+
+        return dataFromExcel;
+    }
+}
